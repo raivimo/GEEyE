af904e48636d1914eb3001a56ae6a5e093dc3693
a033565458v <a033565458v@S09-PC22>
2022-04-04 17:44:49 +0200

    04/04/2022

--- GEEyE/src/geeye/Evento.java
+++ GEEyE/src/geeye/Evento.java
@@ -7,33 +7,18 @@ import java.util.Scanner;
  * @author Ian, Cristina y Raimon.
  */
 public class Evento {
-    //Variables
-    private int maxClientes = 100;
-    private int maxEspacios = 5;
-    private int maxReservas = 100;
-    private int cuantosEspacio = 0;
-    private int cuantosCliente = 0;
-    private int cuantosReserva = 0;
-    
+          
     //ArrayList de Objetos
-    ArrayList<Cliente> ArrayCliente = new ArrayList();
+    ArrayList<Cliente> ArrayClientes = new ArrayList();
     ArrayList<Espacio> ArrayEspacios = new ArrayList();
-    ArrayList<Reserva> ArrayReserva = new ArrayList();
+    ArrayList<Reserva> ArrayReservas = new ArrayList();
        
-         
-    //Arrays de Objetos.
-    Reserva[] arrayReservas = new Reserva[maxReservas];
-    Cliente[] arrayClientes = new Cliente[maxClientes];
-    Espacio[] arrayEspacios = new Espacio[maxEspacios];
-    
     //Constructor
-    
     public Evento() {    
         ArrayEspacios.add(new Espacio ("Cartuja", 500, true));
         ArrayEspacios.add(new Espacio ("Vallesa de Mandor", 150, false));
         ArrayEspacios.add(new Espacio ("Palacio de los Congresos", 50, true));
         ArrayEspacios.add( new Espacio ("Hotel Las Arenas", 100, true));
-        cuantosEspacio = 4;
     }
     
     //Metodos
@@ -49,7 +34,6 @@ public class Evento {
             encontrados.get(i).imprimirEspacio();
         }
         return encontrados;
-        
     }
     
     
@@ -80,91 +64,90 @@ public class Evento {
     }
     
     public void verClientes(){
-        if (cuantosCliente == 0)
+        if ( ArrayClientes.isEmpty() )
             System.out.println("No hay Clientes registrados todavia");
         else{
-            for (int i = 0; i < cuantosCliente; i++) {
+            for (int i = 0; i < ArrayClientes.size() ; i++) {
                 System.out.print(i+" . ");
-                arrayClientes[i].imprimirCliente();
+                ArrayClientes.get(i).imprimirCliente();
             }
         }
     }
     
     public void anyadirCliente(String nombre, String apellidos, int numCliente, String direccion, String telf){
-        if( cuantosCliente > 100)
-            System.err.println("Máximo de clientes alcanzado");
-        else{
-            arrayClientes[cuantosCliente] = new Cliente (nombre, apellidos, numCliente, direccion, telf);
-            cuantosCliente++;
-        }
+        ArrayClientes.add( new Cliente (nombre, apellidos, numCliente, direccion, telf) );      
     }
     
     public void eliminarCliente(int borrar){
-        if ( borrar >= 0){
-            for (int i = borrar +1; i <= cuantosCliente; i++) {
-                arrayClientes[i - 1] = arrayClientes[i];
+        if ( borrar >= 0 && borrar <= ArrayClientes.size() ){
+            for (int i = borrar ; i <= ArrayClientes.size() ; i++) {
+                ArrayClientes.remove(borrar);
             }
-            cuantosCliente --;
         }
-        else 
+        else //Lanzar excepcion
             System.err.println("POSICIÓN INCORRECTA");
         System.out.println("Cliente en la posición: "+ borrar+ " borrado.");
     }
         
     public void verReservas(){
-        if (cuantosReserva == 0)
+        if ( ArrayReservas.isEmpty() )
             System.out.println("No hay reservas pendientes");
         else{
-            for (int i = 0; i < cuantosReserva; i++) {
-                System.out.print("POSICION: " + i + " ");
-                arrayReservas[i].imprimir();
+            for (int i = 0; i < ArrayReservas.size() ; i++) {
+                System.out.print("Posición: " + i + " ");
+                ArrayReservas.get(i).imprimir();
             }
         }
     }
     
     public void anyadirReserva(String fechaReserva, int idReserva, int numComensales, boolean pagado, Espacio arrayEspacios[]){
         Scanner in = new Scanner(System.in);
-        if(cuantosReserva < maxReservas){
-            for (int i = 0; i < cuantosEspacio ; i++) {
-                if(arrayEspacios[i].getAforo() >= numComensales){
+        for (int i = 0; i < ArrayReservas.size() ; i++) {
+            if(ArrayEspacios.get(i).getAforo() >= numComensales){
                     System.out.print("Num. Espacio: " + i + " .");
-                    arrayEspacios[i].imprimirEspacio();
-                }
+                    ArrayEspacios.get(i).imprimirEspacio();
             }
-            System.out.println("Elige numero de espacio");
-            int numEspacio = in.nextInt();
-            arrayReservas[cuantosReserva]= new Reserva(fechaReserva, idReserva, numComensales, pagado, arrayEspacios[numEspacio]);
-            cuantosReserva++;
         }
+        System.out.println("Elige numero de espacio");
+        int numEspacio = in.nextInt();
+        ArrayReservas.add(new Reserva(fechaReserva, idReserva, numComensales, pagado, arrayEspacios[numEspacio]));
     }
            
     public void eliminarReserva(int borrarReserva){
-        if ( borrarReserva >= 0 && borrarReserva <= cuantosReserva ){
-            for (int i = borrarReserva +1; i <= cuantosReserva; i++) {
-                arrayReservas[i - 1] = arrayReservas[i];
+        if ( borrarReserva >= 0 && borrarReserva <= ArrayReservas.size()  ){
+            for (int i = borrarReserva; i <= ArrayReservas.size() ; i++) {
+                ArrayReservas.remove(borrarReserva);
             }
-            cuantosReserva --;
         }
         else 
-            System.err.println("POSICIÓN INCORRECTA");
+            System.err.println("POSICIÓN INCORRECTA");//Lanzar Excepcion
         System.out.println("Reserva en la posición: " + borrarReserva + " borrada.");
     }
 
-    public void buscarCliente(String nombre){
-        for (int i = 0; i < cuantosCliente; i++) {
-            if (arrayClientes[i].getNombre().equalsIgnoreCase(nombre)) {
-                arrayClientes[i].imprimirCliente();
-            }
+    public ArrayList<Cliente> buscarCliente(String nombre){
+        //ArrayList encontrados (temporal, muestra coincidencias con nombre)
+        ArrayList<Cliente> encontrados = new ArrayList();
+        for (int i = 0; i < ArrayClientes.size(); i++) {
+            if ( ArrayClientes.get(i).getNombre().toUpperCase().contains(nombre.toUpperCase()) )
+                encontrados.add(ArrayClientes.get(i));
         }
+        for (int i = 0; i < encontrados.size(); i++) {
+            System.out.print("Posicion: " + i + " ");
+            encontrados.get(i).imprimirCliente();
+        }
+        return encontrados;
+        
     }
+
     
-    public void buscarReserva(int idReserva){
-        for (int i = 0; i < cuantosReserva; i++) {
-            if(arrayReservas[i].getIdReserva()==idReserva)
-                arrayReservas[i].imprimir();
+    public ArrayList<Reserva> buscarReserva(int idReserva){
+        for (int i = 0; i < ArrayReservas.size() ; i++) {
+            if(ArrayReservas.get(i).getIdReserva() == idReserva)
+                ArrayReservas.get(i).imprimir();
             else
-                System.out.println("ID no encontrado.");
+                System.out.println("ID no encontrado.");//Lanzar excepcion
         }
+        return encontrados;
     }
 
     
